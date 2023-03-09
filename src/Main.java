aa4a582165b41c3afa84929eb9cce342f51b057b
Arzije <arzije.ziberovska@gmail.com>
2023-03-09 10:29:16 +0100

    Skapat ett JSON object och skrivit och hömtat data via den

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,19 @@
+import org.json.simple.JSONObject;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("This is an example");
+
+        // Skapa ett JSON object
+        JSONObject jsonOb = new JSONObject();
+
+        //Spara väeswn u JSONobject
+        jsonOb.put("namn", "Marcus");
+        jsonOb.put("age", 34);
+
+        //Skriva ut värden
+        System.out.println("Mitt namn är " + jsonOb.get("namn"));
+        System.out.println("Jag är " + jsonOb.get("age") + " år gammal");
     }
 }
